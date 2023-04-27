1139d12ab283e6d663ed3125e7f865e6dae70c26
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-04-27 14:26:31 -0300

    Feat: Refatorando o service dos carros" ""

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -3,6 +3,11 @@ import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 class CarService {
+  private carODM: CarODM;
+
+  constructor() {
+    this.carODM = new CarODM();
+  }
   private createCarDomain(car: ICar | null) {
     if (car) {
       return new Car(car);
@@ -10,32 +15,29 @@ class CarService {
   }
 
   public async insertOneCar(car: ICar): Promise<Car | undefined> {
-    const carODM = new CarODM();
-    const result = await carODM.create(car);
+    const result = await this.carODM.create(car);
 
     return this.createCarDomain(result);
   }
 
   public async findAll(): Promise<(Car | undefined)[] | undefined> {
-    const carODM = new CarODM();
-    const result = await carODM.findAll();
+    const result = await this.carODM.findAll();
     const carList = result?.map((car) => this.createCarDomain(car));
     return carList;
   }
 
   public async findById(id: string): Promise <Car | undefined> {
-    const carODM = new CarODM();
-    const result = await carODM.findById(id);
+    const result = await this.carODM.findById(id);
 
     return this.createCarDomain(result);
   }
 
   public async updateOne(id: string, car: ICar):Promise <Car | undefined> {
-    const carODM = new CarODM();
-    const result = await carODM.update(id, car);
+    const result = await this.carODM.update(id, car);
 
     return this.createCarDomain(result);
   }
+  
   public async removeOne(id:string): Promise <Car | undefined> {
     const carODM = new CarODM();
     const result = await carODM.remove(id);
